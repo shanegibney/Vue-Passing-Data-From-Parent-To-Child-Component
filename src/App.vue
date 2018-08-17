96aaf1d94db20a4fff910b7d72bcccaf01d87fa9
shanegibney <shanegibney@gmail.com>
2018-08-17 23:07:55 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <h1>I am the parent component</h1>
+    <Child :people="people"/>
+  </div>
+</template>
+
+<script>
+import Child from './components/Child'
+export default {
+  name: 'App',
+  components: {
+    Child
+  },
+  data(){
+    return {
+      title: 'People',
+      people: [
+        {name: 'Joe', qualified: 'false', reg: '123'},
+        {name: 'Jill', qualified: 'true', reg: '231'},
+        {name: 'June', qualified: 'false', reg: '312'},
+        {name: 'Jack', qualified: 'true', reg: '132'}
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Child.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+  <div>
+    <h1>This is the child component</h1>
+    <div v-for="(individual, index) in people" :key="index">
+      <span>{{ index+1 }}.   {{ individual.name }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Child',
+  props: ['people'],
+  data(){
+    return {}
+  }
+}
+</script>
+
+<style lang="css">
+h1 {
+   color: #444;
+}
+</style>
